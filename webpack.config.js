06d218bb3d5c66c9bdb08ea05ab32723339d2f24
Guy home <guyi@hereo.co.uk>
2017-09-25 21:05:01 +0300

    Added css  and scss

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,17 @@ var config = {
                     presets: ["react", "es2015", "stage-2"]
                 }
             }
-        ]
+        ],
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
     }
 };
 
