e09d6b5a40b051aaed16e649eb61c7cbffb818f7
Guy home <guyi@hereo.co.uk>
2017-09-27 05:12:28 +0300

    addes api

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class Home extends React.Component {
     render() {
+     var data =this.props.data
         return (
-            <div><p>In a new compents</p></div>
+            
+            <div><p>hi my this is  I am   </p></div>
         );
     }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,34 @@
+// lib
 import React from 'react'
 import ReactDOM from 'react-dom'
+// components
 import  Header  from "./components/Header";
 import  Home  from "./components/Home";
+// other
+import {callApi} from "./js/api"
+
 
 
 class App extends React.Component {
+    
+    
+    componentDidMount(){
+        
+                callApi(function(data){
+                  console.log(data)
+                  delta(data)
+                }) 
+                
+                 var delta = (data) => {
+                    this.setState({
+                        data  : data 
+                    });
+                }
+            }
+    
     render() {
+         let data = this.state.data
+console.log(this.state.data)
         return (
             <div className="container">
                 <div className="row">
@@ -14,7 +37,7 @@ class App extends React.Component {
                     </div>
                 </div>
                 <div className="row">
-                    <Home/>
+                    <Home />
                     <div className="col-xs-10 col-xs-offset-1">
                     </div>
                 </div>
